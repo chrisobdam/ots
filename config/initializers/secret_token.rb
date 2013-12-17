41924dbf245173ef8fcb4c60caf945387a4b7255
Chris Obdam <chrisobdam@MacBook-Pro-van-Chris.local>
2013-12-17 21:32:13 +0100

    Naked Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ots::Application.config.secret_key_base = 'c498eb24d40d19f7dc398e6ba8a41d5fecb7708f6d7e33c29e00353d2bec5f1b807eebbdeb2da7a916977bce02a42dec44673a591973eaf370431fe4abb608d3'
