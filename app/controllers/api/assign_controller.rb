2ec5249ca7a325dd3f75acde0f92c4df61c7ef4a
Chris Obdam <chrisobdam@MacBook-Pro-van-Chris.local>
2014-01-22 00:13:47 +0100

    Check for assign flow

--- app/controllers/api/assign_controller.rb
+++ app/controllers/api/assign_controller.rb
@@ -11,12 +11,60 @@ private
     unless params[:task].present?
       render text: "No task object found", status: :unprocessable_entity; return false
     end
-    unless params[:task][:owner].present?
-      render text: "No owner found", status: :unprocessable_entity; return false
+    unless params[:task][:identifier].present?
+      render text: "No identifier found", status: :unprocessable_entity; return false
     end
     unless params[:task][:owner].present?
       render text: "No owner found", status: :unprocessable_entity; return false
     end
+    unless params[:task][:token].present?
+      render text: "No token found", status: :unprocessable_entity; return false
+    end
+    unless params[:task][:title].present?
+      render text: "No title found", status: :unprocessable_entity; return false
+    end
+    
+    unless has_assignees and has_listeners
+      render text: "No assignee or listener found", status: :unprocessable_entity; return false
+    end
+    
+    unless check_for_valid_user
+      render text: "No assignee or listener in this task is known on this server", status: :not_found; return false
+    end
+  end
+  
+  def has_assignees  
+    result = false
+    if params[:task][:assignees].present?
+      if params[:task][:assignees].is_a?(Array)
+        params[:task][:assignees].each |assignee| do
+          unless assignee[:identifer].present?
+            render text: "No identifier found in assignee", status: :unprocessable_entity; return false
+          end
+        end
+        result = true
+      else
+        render text: "Malformed assignees; array expected", status: :unprocessable_entity; return false
+      end
+    end
+    result
+  end
+
+  def has_listeners
+    result = false
+    if params[:task][:listeners].present?
+      if params[:task][:listeners].is_a?(Array)
+        params[:task][:listeners].each |listener| do
+          unless listener.is_a?(String)
+            render text: "Malformed listeners; array of strings expected", status: :unprocessable_entity; return false
+          end
+        end
+        result = true
+      else
+        render text: "Malformed listeners; array expected", status: :unprocessable_entity; return false
+      end
+    end
+    result
   end
   
   def get_user
@@ -26,5 +74,13 @@ private
       render text: "No user found", status: :not_found
       return false      
     end
+  end
+  
+  def check_for_valid_user
+    result = []
+    (params[:task][:listeners] + params[:task][:assignees].map{|x|x[:identifier]}).each |user| do
+      result << obj if (obj = User.find_by(identifier: user))
+    end
+    !result.empty?
   end
 end
